913f88aa8e1a0f79c1581e8af3cdd2e4c6d0ad42
dezhonger <373440955@qq.com>
2017-05-08 10:58:43 +0800

    modify sql

--- sql_table/table.sql
+++ sql_table/table.sql
@@ -1,15 +1,29 @@
-CREATE TABLE `mapi_user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自动编号',
-  `username` varchar(100) NOT NULL DEFAULT '' COMMENT '用户名',
-  `nickname` varchar(100) NOT NULL DEFAULT '' COMMENT '昵称',
-  `password` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
-   `email` varchar(50) NOT NULL DEFAULT '' COMMENT '邮箱',
-  `last_login_time` datetime NOT NULL COMMENT '最后登录时间',
-  `last_login_ip` varchar(30) NOT NULL DEFAULT '' COMMENT '最后登录IP',
-  `is_lock` CHAR(1) NOT NULL DEFAULT 'N' COMMENT '是否锁定N：未锁定 Y：锁定',
-  `head_image` varchar(100) NOT NULL DEFAULT '' COMMENT '用户头像',
-  `create_time` datetime DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_mapi_user_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1DEFAULT CHARSET=utf8 COMMENT='用户表';
+﻿用户基本信息表:
+CREATE TABLE `user` (
+    `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自动编号',
+    `nickname` VARCHAR (256) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户昵称',
+    `password` CHAR (100) COLLATE utf8mb4_bin DEFAULT NULL,
+    `email` VARCHAR (64) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户email地址',
+    `status` VARCHAR (16) COLLATE utf8mb4_bin NOT NULL DEFAULT 'active' COMMENT '用户状态：active/locked',
+    `head_image` VARCHAR (1024) COLLATE utf8mb4_bin DEFAULT NULL,
+    `bind_phone` VARCHAR (16) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '绑定手机',
+    `last_login_time` datetime NOT NULL COMMENT '最后登录时间',
+    `last_login_ip` VARCHAR (15) COLLATE utf8mb4_bin NOT NULL COMMENT '最后登录IP地址',
+    `is_notfiy_sign` CHAR (1) COLLATE utf8mb4_bin DEFAULT 'Y' COMMENT '是否开启签到提醒',
+    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
+
+
+用户登录日志表:
+CREATE TABLE `user_login_log` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自动编号',
+  `user_id` int(11) DEFAULT NULL,
+  `platform` varchar(50) DEFAULT NULL,
+  `device_number` varchar(50) DEFAULT NULL,
+  `login_ip` varchar(50) DEFAULT NULL,
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
