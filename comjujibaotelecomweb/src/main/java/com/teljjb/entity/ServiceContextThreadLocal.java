942cea43da826ec31cbc67a26c411d9f6db8ce5b
dezhonger <373440955@qq.com>
2017-05-18 22:40:02 +0800

    add

--- comjujibaotelecomweb/src/main/java/com/teljjb/entity/ServiceContextThreadLocal.java
+++ comjujibaotelecomweb/src/main/java/com/teljjb/entity/ServiceContextThreadLocal.java
@@ -1,16 +1,5 @@
 package com.teljjb.entity;
 
-/**
- * @Filename ServiceContextThreadLocal.java
- * @Description
- * @Version 1.0
- * @Author lingmao
- * @Email 162283610@qq.com
- * @History <li>Author: lingmao</li>
- * <li>Date: 2016年4月7日</li>
- * <li>Version: 1.0</li>
- * <li>Content: create</li>
- */
 public class ServiceContextThreadLocal {
 
     public static final ThreadLocal<ServiceContext> threadLocal = new ThreadLocal<ServiceContext>() {
