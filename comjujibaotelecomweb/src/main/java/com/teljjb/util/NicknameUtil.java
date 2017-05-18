6bb94e06271aa81a3d2fd67e04e95deb11e59b8a
dezhonger <373440955@qq.com>
2017-05-18 21:47:10 +0800

    fix

--- comjujibaotelecomweb/src/main/java/com/teljjb/util/NicknameUtil.java
+++ comjujibaotelecomweb/src/main/java/com/teljjb/util/NicknameUtil.java
@@ -4,8 +4,8 @@ package com.teljjb.util;
  * Created by dezhonger on 2017/5/12.
  */
 public final class NicknameUtil {
-    public static boolean checkNickname(String nickname) {
-        if(StringUtil.isEmpty(nickname) || nickname.length() <= 3) {
+    public static boolean   checkNickname(String nickname) {
+        if(StringUtil.isEmpty(nickname) || nickname.length() <= 0) {
             return false;
         }
         return true;
